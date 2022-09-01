0379a2df69fd69c7143a7043eb17d1127433f18b
Leandro Alencar <zzleandrobritozz@gmail.com>
2022-09-01 16:17:06 -0300

    updates

--- pages/dashboard.tsx
+++ pages/dashboard.tsx
@@ -1,9 +1,15 @@
 /* eslint-disable react-hooks/rules-of-hooks */
-import { useContext } from "react"
+import { useContext, useEffect } from "react"
 import { AuthContext } from "../contexts/AuthContext"
+import { api } from "../services/api"
 
-export default function dashboard() {
+export default function Dashboard() {
   const { user } = useContext(AuthContext)
+
+  useEffect(() => {
+    api.get('/me').then(response => console.log(response))
+  }, [])
+
   return (
     <h1>DashBoard: {user?.email}</h1>
   )
